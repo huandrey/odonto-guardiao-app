8c14c584a8d8b2dfd52af5bc9eacc956b19d3f98
huandrey <huandrey.pontes@ccc.ufcg.edu.br>
2025-04-22 06:09:29 -0300

    add image to documents card

--- src/features/documentos-norteadores/components/documentos-norteadores-content.tsx
+++ src/features/documentos-norteadores/components/documentos-norteadores-content.tsx
@@ -4,6 +4,7 @@ import legalDocuments from '../utils/legal-documents.json'
 import { Header } from '../../../shared/components/header/components';
 import { useNavigate } from 'react-router-dom';
 import { Footer } from '../../../shared/components/footer';
+import eca from '../../../assets/eca.png'
 
 interface LegalDocument {
   title: string;
@@ -14,6 +15,7 @@ interface LegalDocument {
   }[];
   url: string;
   localPath: string;
+  image: string;
 }
 
 const DocumentosNorteadoresContent: React.FC = () => {
@@ -115,13 +117,14 @@ const DocumentosNorteadoresContent: React.FC = () => {
             {(legalDocuments as LegalDocument[]).map((doc, index) => (
               <div className="legaldoc-document-card" key={index}>
                 <div className="legaldoc-document-icon">
-                  <svg width="48" height="48" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <img src={doc.image} style={{ width: '100%'}}/>
+                  {/* <svg width="48" height="48" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <path d="M14 2H6C5.46957 2 4.96086 2.21071 4.58579 2.58579C4.21071 2.96086 4 3.46957 4 4V20C4 20.5304 4.21071 21.0391 4.58579 21.4142C4.96086 21.7893 5.46957 22 6 22H18C18.5304 22 19.0391 21.7893 19.4142 21.4142C19.7893 21.0391 20 20.5304 20 20V8L14 2Z" stroke="#FBC02D" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
                     <path d="M14 2V8H20" stroke="#FBC02D" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
                     <path d="M16 13H8" stroke="#FBC02D" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
                     <path d="M16 17H8" stroke="#FBC02D" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
                     <path d="M10 9H9H8" stroke="#FBC02D" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
-                  </svg>
+                  </svg> */}
                 </div>
                 <div className="legaldoc-document-content">
                   <h3 className="legaldoc-document-title">{doc.title}</h3>
